7cd5fdd20a6e603649a372fdb07abcea9262ac40
Khushbu Islam Misbah <khushbuislamoffice@gmail.com>
2021-04-02 23:33:43 +0600

    update

--- index.js
+++ index.js
@@ -23,47 +23,65 @@ app.get('/', (req, res) => {
 
 client.connect(err => {
   const productsCollection = client.db("freshMartDb").collection("products");
-    console.log('database connected successfully');
+  console.log('database connected successfully');
 
-    //post api for data load
-    app.post('/addProduct', (req, res) => {
-      const newProduct = req.body;
-      console.log('added new product:', newProduct);
-      productsCollection.insertOne(newProduct)
+  //post api for data load
+  app.post('/addProduct', (req, res) => {
+    const newProduct = req.body;
+    console.log('added new product:', newProduct);
+    productsCollection.insertOne(newProduct)
       .then(result => {
         // console.log('inserted Count',result.insertedCount)
         res.send(result.insertedCount > 0)
       })
-    });
+  });
 
-    //get api for display data
-    app.get('/products', (req, res) => {
-      productsCollection.find()
+  //get api for display data
+  app.get('/products', (req, res) => {
+    productsCollection.find()
       .toArray((err, products) => {
         res.send(products);
         // console.log('from database',products);
       })
-    });
-    
-    //get api for checkout
-    app.get('/checkedProduct/:productId', (req, res) => {
-      console.log(req.params.productId)
-      productsCollection.find({_id: ObjectID(req.params.productId)})
+  });
+
+  //get api for checkout
+  app.get('/checkedProduct/:productId', (req, res) => {
+    console.log(req.params.productId)
+    productsCollection.find({ _id: ObjectID(req.params.productId) })
       .toArray((err, item) => {
         res.send(item);
       })
-    })
+  })
 
-    //delete api for delete data
-    app.delete('/deleteProduct/:id', (req, res) => {
-      console.log(req.params.id);
-      // const id = ObjectID(req.params.id);
-      // console.log('delete this', id);
-      // productsCollection.findOneAndDelete({_id: id})
-      // .then(((err, documents) => {
-      //   res.send(!! documents.value);
-      // })
-    })
+  app.get('/order', (req, res) => {
+    productsCollection.find()
+      .toArray((err, documents) => {
+        res.send(documents);
+      })
+  });
+
+  //post api create order
+  app.post('/addOrder', (req, res) => {
+    const order = req.body;
+    // console.log(products);
+    ordersCollection.insertOne(order)
+      .then(result => {
+        // console.log(result.insertedCount);
+        res.send(result.insertedCount > 0);
+      })
+  });
+
+//delete api for delete data
+app.delete('/deleteProduct/:id', (req, res) => {
+  console.log(req.params.id);
+  // const id = ObjectID(req.params.id);
+  // console.log('delete this', id);
+  // productsCollection.findOneAndDelete({_id: id})
+  // .then(((err, documents) => {
+  //   res.send(!! documents.value);
+  // })
+})
 });
 
 app.listen(port)
